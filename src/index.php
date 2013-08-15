e3b48d58383f5722b065e5a63cecb666e6bac1ff
Amoubine <moubine.anass@live.fr>
2013-08-15 16:04:10 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
  require_once("facebook.php");
 
   $config = array();
-  $config['appId'] = 'YOUR_APP_ID';
-  $config['secret'] = 'YOUR_APP_SECRET';
+  $config['appId'] = '1400712613485469';
+  $config['secret'] = 'd97ea260514f97d7b320a52890f72427';
   $config['fileUpload'] = false; // optional
 
   $facebook = new Facebook($config);
